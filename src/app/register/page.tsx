832eb6d181a2c01faa7e82b0e0514f1c9d2b2d39
Lejitop <alejocrrs@gmail.com>
2024-07-24 21:50:52 -0500

    chore: Update Next.js font import in layout.tsx

---
+++ src/app/register/page.tsx
@@ -0,0 +1,40 @@
+export default function Register() {
+  return (
+    <></>
+    // <div className="layout flex flex-col items-center justify-center">
+    //   <h1 className="app-title">RT Chat</h1>
+
+    //   <form className="form form--login">
+    //     <h2 className="form__title">Sign in</h2>
+
+    //     <div className="form__control">
+    //       <input
+    //         type="text"
+    //         placeholder="Username"
+    //         className="form__input"
+    //         id="username"
+    //       />
+    //       <UserIcon className="form__icon" width={32} height={32} />
+    //     </div>
+
+    //     <div className="form__control">
+    //       <input
+    //         type="password"
+    //         placeholder="Password"
+    //         className="form__input"
+    //         id="password"
+    //       />
+    //       <KeyIcon className="form__icon" width={32} height={32} />
+    //     </div>
+
+    //     <div className="form__control form__control--button">
+    //       <button className="form__button">Join chat!</button>
+
+    //       <a className="form__link" href="/register">
+    //         Don&apos;t have an account? Click here!
+    //       </a>
+    //     </div>
+    //   </form>
+    // </div>
+  );
+}
